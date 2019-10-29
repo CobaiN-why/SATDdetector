5da7546e6b278c7f1ae268d843a3396ff54d3a50
ThJoker <717898675@qq.com>
2019-10-29 18:56:54 +0800

    add project version controller to support parse of different version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length == 0) {
+        if (args.length != 2) {
             printHelp();
             return;
         }
